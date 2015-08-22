7accfcbed5cabcd7ea08b33fd727faeea0acf6bb
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2015-08-23 04:50:11 +0900

    add .bash_profile

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 
 CURDIR=$(cd $(dirname ${0}) && pwd)
 
+ln -s ${CURDIR}/.bash_profile ${HOME}/
 ln -s ${CURDIR}/.bashrc ${HOME}/
 ln -s ${CURDIR}/.emacs.d ${HOME}/
 ln -s ${CURDIR}/.gitconfig ${HOME}/
