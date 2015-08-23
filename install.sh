4a34f98152574d8ad68e3d72bfa8f6a3cd15edf7
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2015-08-24 06:53:18 +0900

    change emacs making data dir

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ ln -s ${CURDIR}/.bashrc ${HOME}/
 ln -s ${CURDIR}/.emacs.d ${HOME}/
 ln -s ${CURDIR}/.gitconfig ${HOME}/
 ln -s ${CURDIR}/.gitignore_global ${HOME}/
-ln -s ${CURDIR}/.ddskk ${HOME}/
 ln -s ${CURDIR}/.Brewfile ${HOME}/
 ln -s ${CURDIR}/.latexmk ${HOME}/
 ln -s ${CURDIR}/.zshrc ${HOME}/
