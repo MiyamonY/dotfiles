fd6474483a719d5883c26329c973f37a5a566fd1
Yohei MIYAMOTO <miyamoto@xyzzyx.co.jp>
2015-08-24 05:48:34 +0900

    add .gitignore_global

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ ln -s ${CURDIR}/.bash_profile ${HOME}/
 ln -s ${CURDIR}/.bashrc ${HOME}/
 ln -s ${CURDIR}/.emacs.d ${HOME}/
 ln -s ${CURDIR}/.gitconfig ${HOME}/
+cp ${CURDIR}/.gitconfig.local ${HOME}/
+ln -s ${CURDIR}/.gitignore_global ${HOME}/
 ln -s ${CURDIR}/.ddskk ${HOME}/
 ln -s ${CURDIR}/.Brewfile ${HOME}/
 ln -s ${CURDIR}/.latexmk ${HOME}/
 ln -s ${CURDIR}/.zshrc ${HOME}/
+
+echo $'\e[1;31m edit .gitconfig.local \e[m'
