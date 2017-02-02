c9a8cde0aba8bc7e469fb813bbf313bc71cbce0a
MIYAMOTO Yohei <miyamoto@xyzzyx.co.jp>
2017-02-02 11:38:59 +0900

    add

---
+++ .emacs.d/conf/insert/01:.*.h
@@ -0,0 +1,21 @@
+/**
+ * File:  `(file-name-nondirectory (buffer-file-name))`
+ * Author: ymiyamoto
+ *
+ * Created on `(current-time-string)`
+ */
+
+#ifndef ${1:_`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H_}
+#define $1
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+$0
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* $1 */
