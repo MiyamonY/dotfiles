6b23928a94cdd5f37ccca559a8b7bba27e2c1320
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-02-28 23:12:51 +0900

    [add] byobuの設定を追加

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,6 +2,10 @@
 
 CURDIR=$(cd $(dirname ${0}) && pwd)
 
+if [ ! -e ${HOME}/.config ]; then
+    mkdir ${HOME}/.config
+fi
+
 ln -s ${CURDIR}/.bash_profile ${HOME}/
 ln -s ${CURDIR}/.bashrc ${HOME}/
 ln -s ${CURDIR}/.emacs.d ${HOME}/
@@ -12,6 +16,7 @@ ln -s ${CURDIR}/.latexmkrc ${HOME}/
 ln -s ${CURDIR}/.zshrc ${HOME}/
 ln -s ${CURDIR}/.Xmodmap ${HOME}/
 ln -s ${CURDIR}/.keysnail.js ${HOME}/
+ln -s ${CURDIR}/byobu ${HOME}/.config/
 
 if [ ! -e ${HOME}/.gitconfig.local ]; then
    cp ${CURDIR}/.gitconfig.local ${HOME}/
