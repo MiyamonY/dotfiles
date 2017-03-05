057dfb6d5a2b2bfdff0f19617faf149bd23ba684
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-03-06 00:10:27 +0900

    [fix] マスターウィンドウを拡大・縮小するkeybindを修正
    * Meta+jで拡大, Meta+kで縮小に

--- awesome/descs.lua
+++ awesome/descs.lua
@@ -2,5 +2,7 @@ local descs = {}
 
 descs.FOCUS_NEXT = "次のウィンドウへ"
 descs.FOCUS_PREV = "前のウィンドウへ"
+descs.INCREASE_MASTER_WIDTH = "マスターウィンドウを拡大"
+descs.DECREASE_MASTER_WIDTH = "マスターウィンドウを縮小"
 
 return descs
