1813abcb16c1ea9aa98ae8326d0f134a0907aefc
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-03-07 11:30:34 +0900

    [add] gtkrc-2.0を追加

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -21,6 +21,7 @@ ln -s ${CURDIR}/.zshrc ${HOME}/
 ln -s ${CURDIR}/.Xmodmap ${HOME}/
 ln -s ${CURDIR}/.keysnail.js ${HOME}/
 ln -s ${CURDIR}/.Xresources ${HOME}/
+ln -s ${CURDIR}/.gtkrc-2.0 ${HOME}/
 ln -s ${CURDIR}/config.fish ${HOME}/.config/fish/
 ln -s ${CURDIR}/byobu ${HOME}/.config/
 ln -s ${CURDIR}/awesome ${HOME}/.config/
