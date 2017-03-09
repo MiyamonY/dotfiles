02814e66d72f8e9c36b10144413bf37d0b35810e
MIYAMOTO Yohei <miyamoto@xyzzyx.co.jp>
2017-03-09 20:18:00 +0900

    [add][awesome] 色をdraculaテーマに変更

---
+++ awesome/color.lua
@@ -0,0 +1,17 @@
+local color = {}
+
+-- dracula color theme
+color.BACKGROUND = "#282a36"
+color.CURRENTLINE = "#44475a"
+color.SELECTION = "#44475a"
+color.FOREGROUND = "#f8f8f2"
+color.COMMENT = "#6272a4"
+color.CYAN = "#8be9fd"
+color.GREEN = "#50fa7b"
+color.ORANGE = "#ffb86c"
+color.PINK = "#ff79c6"
+color.PURPLE = "#bd93f9"
+color.RED = "#ff5555"
+color.YELLOW = "#f1fa8c"
+
+return color
