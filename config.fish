5281ec0c0926aa998167f8bd7dd6756364786f58
MIYAMOTO Yohei <miyamoto@xyzzyx.co.jp>
2017-05-01 11:38:26 +0900

    [add][fish] pyenvの設定を追加

--- config.fish
+++ config.fish
@@ -40,3 +40,9 @@ function sync_history --on-event fish_preexec
   history --save
   history merge
 end
+
+## pyenv
+set -x PATH $PATH $HOME/.pyenv/bin
+if test $HOME/.pyenv/bin/pyenv
+  status --is-interactive; and source (pyenv init -|psub)
+end
