4e668f8247229b5bc64c12b76d12fc3981561523
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-04 09:43:44 +0900

    [add][emacs] racket用のテンプレートファイルを追加

---
+++ .emacs.d/conf/insert/50:.*.rkt
@@ -0,0 +1,12 @@
+;;;
+;; File:  `(file-name-nondirectory (buffer-file-name))`
+;; Author: ymiyamoto
+;;
+;; Created on `(current-time-string)`
+;;
+#lang racket
+
+
+(module+ test
+  (require rackunit)
+  )
