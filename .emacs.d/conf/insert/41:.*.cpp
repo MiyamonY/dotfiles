1318424ddc98bbcc1d17dbe8cb8822a3d05a4fee
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-16 08:05:29 +0900

    [add][emacs] c++用のテンプレート更新

--- .emacs.d/conf/insert/41:.*.cpp
+++ .emacs.d/conf/insert/41:.*.cpp
@@ -4,7 +4,18 @@
 //
 // Created on `(current-time-string)`
 //
-
-#include <cstdint>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+#define rep(_n, _init, _N) for (int32_t _n = _init; _n < (int32_t)(_N); _n++)
+#define rrep(_n, _N, _end) for (int32_t _n = (int32_t)(_N); _n >= _end; _n--)
+#define iceil(_x, _y) (((_x) + (_y)-1) / (_y))
+#define ifloor(_x, _y) ((_x) / (_y))
+
+static const double pi = 2 * asin(1);
+static const double precision = 10e-9;
+
+int32_t main()
+{
+  return 0;
+}
