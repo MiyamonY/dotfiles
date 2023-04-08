9497346fc976ab031955f6101a6bd49784056977
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2023-04-09 07:51:34 +0900

    add: use stow in install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,40 +1,12 @@
 #!/bin/sh
 
-CURDIR=$(cd $(dirname ${0}) && pwd)
-
-if [ ! -e ${HOME}/.config ]; then
-    mkdir ${HOME}/.config
+if [ ! -f /usr/bin/stow ]; then
+   sudo pacman -U https://archive.archlinux.org/packages/s/stow/stow-2.2.2-5-any.pkg.tar.xz
 fi
 
-if [ ! -e ${HOME}/.config/fish ]; then
-    mkdir ${HOME}/.config/fish
-fi
+stow -R -v -d . -t ~/.config starship alacritty stumpwm fish
+stow -R -v -d . -t ~ tmux bash bash git xmodmap xwindow utop
 
-if [ ! -e ${HOME}/.config/alacritty ]; then
-    mkdir ${HOME}/.config/alacritty
-fi
-
-stow -R -v -d . -t ~/.config starship
-
-ln -s ${CURDIR}/.bash_profile ${HOME}/
-ln -s ${CURDIR}/.bashrc ${HOME}/
 ln -s ${CURDIR}/.emacs.d ${HOME}/
-ln -s ${CURDIR}/.gitconfig ${HOME}/
-ln -s ${CURDIR}/.gitignore_global ${HOME}/
-ln -s ${CURDIR}/.latexmkrc ${HOME}/
-ln -s ${CURDIR}/.Xmodmap ${HOME}/
-ln -s ${CURDIR}/.Xresources ${HOME}/
-ln -s ${CURDIR}/.gtkrc-2.0 ${HOME}/
-ln -s ${CURDIR}/.xinitrc ${HOME}/
-ln -s ${CURDIR}/.xprofile ${HOME}/
-ln -s ${CURDIR}/config.fish ${HOME}/.config/fish/
-ln -s ${CURDIR}/alacritty.yml ${HOME}/.config/alacritty/
-ln -s ${CURDIR}/.tmux.conf ${HOME}/
-ln -s ${CURDIR}/stumpwm ${HOME}/.config/
-ln -s ${CURDIR}/.ocamlinit ${HOME}/
-
-if [ ! -e ${HOME}/.gitconfig.local ]; then
-   cp ${CURDIR}/.gitconfig.local ${HOME}/
-fi
 
 echo $'\e[1;31m edit .gitconfig.local \e[m'
