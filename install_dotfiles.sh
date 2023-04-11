f9c2b332003c1b0f682394761ea69f13b790c4ed
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2023-04-11 23:30:02 +0900

    [st] add: set st as default terminal

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,7 +4,7 @@ if [ ! -f /usr/bin/stow ]; then
    sudo pacman -U https://archive.archlinux.org/packages/s/stow/stow-2.2.2-5-any.pkg.tar.xz
 fi
 
-stow -R -v -d . -t ~/.config starship alacritty stumpwm fish
+stow -R -v -d . -t ~/.config starship stumpwm fish
 stow -R -v -d . -t ~ tmux bash bash git xmodmap xwindow utop
 
 ln -s ${CURDIR}/.emacs.d ${HOME}/
