3136d95a55b7a9fe4bfe0c004d5275dff606308f
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2025-02-28 01:32:48 +0900

    [nix] add: packages

--- flake.nix
+++ flake.nix
@@ -8,14 +8,60 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in
       {
-        packages.${system}.home = nixpkgs.legacyPackages.${system}.buildEnv {
+        packages.${system}.home = pkgs.buildEnv {
           name = "home";
-          paths = with nixpkgs.legacyPackages.${system}; [
+          paths = with pkgs; [
+            # tools
             git
+            hub
             curl
+            wget
+            fzf
+            ripgrep
+            jq
+            docker
+            mise
+            uim
+            # editor
+            emacs
+            vim
+            ghq
+            # shell
+            tmux
+            fish
+            # gui
+            xorg.xrandr
+            xorg.xset
+            xorg.xmodmap
+            arandr
+            stumpwm
+            # browser
+            google-chrome
+            # sql
+            sqlite
+            mariadb
+            mysql-workbench
+            # tableplus
+            # commnunication
+            slack
+            # runtime
+            nodejs_18
+            racket
+            go
+            # aws
+            awscli2
+            ssm-session-manager-plugin
           ];
+        };
+        apps.${system}.upgrade = {
+          type = "app";
+          program = toString (pkgs.writeShellScript "update" ''
+            nix flake update
+            nix profile upgrade home
+          '');
         };
       };
 }
