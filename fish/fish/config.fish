a4f2bc30c8cc68432acdeb480485aeff76f26cf7
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2025-06-17 16:06:41 +0900

    [fish] not to use hub command

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -44,7 +44,6 @@ end
 
 # alias
 alias rm='mv --backup=numbered --target-directory=$HOME/.Trash'
-alias git='hub'
 alias open='xdg-open'
 alias pc='git push -u origin HEAD; and gh pr create -w'
 alias pm='gh pr merge --merge -d'
